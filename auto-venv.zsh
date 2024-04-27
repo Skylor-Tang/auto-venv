a83ea4a1ce7e6a180fb29701a80fa6ce6841e6be
tangmeijian <tang1996mei@gmail.com>
2024-04-27 10:52:28 +0800

    Optimize the implementation of _deactivate_venv:v2

--- auto-venv.zsh
+++ auto-venv.zsh
@@ -1,25 +1,27 @@
 # Check for the existence of a venv directory and activate the Python virtual environment
 # If a venv or .venv directory is found, activate the corresponding virtual environment
 _check_venv() {
-    _deactivate_venv
-    # Check for the existence of a venv or .venv directory
-    if [ -d "venv" ]; then
-        # Activate the venv virtual environment
-        source venv/bin/activate
-        echo "Activated Python virtual environment: venv"
-    elif [ -d ".venv" ]; then
-        # Activate the .venv virtual environment
-        source .venv/bin/activate
-        echo "Activated Python virtual environment: .venv"
-    fi
+    local venv_dirs=("venv" ".venv")
+    for dir in "${venv_dirs[@]}"; do
+        if [ -d "$dir" ]; then
+            source "{$dir}/bin/activate"
+            echo "Activated Python virtual environment: $dir"
+            return
+        fi
+    done
 }
 
 _deactivate_venv() {
     local current_dir="${1-${PWD}}"
-    if [[ -v VIRTUAL_ENV ]] && [[ "${current_dir}" != "$(dirname ${VIRTUAL_ENV})"* ]]; then
+    if [[ -n "$VIRTUAL_ENV" ]] && [[ "${current_dir}" != "${VIRTUAL_ENV%/*}"* ]]; then
         deactivate
-        echo "Deactivated Python virtual environment: $current_dir"
+        echo "Deactivated Python virtual environment: " # TODO
     fi
+}
+
+_auto_venv() {
+    _deactivate_venv
+    check_venv
 }
 # Change directory and automatically check and activate the Python virtual environment
 # This function will call the check_venv() function after changing the directory
@@ -28,5 +30,5 @@ cd() {
     builtin cd "$@"
 
     # Check and activate the Python virtual environment
-    _check_venv
+    _auto_venv
 }
