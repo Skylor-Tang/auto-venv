888eb5d7ff78e9a7e44d8c3e67784b161bc1b4f8
tangmeijian <tang1996mei@gmail.com>
2024-04-27 09:46:40 +0800

    Optimize the implementation of _deactivate_venv

--- auto-venv.zsh
+++ auto-venv.zsh
@@ -1,6 +1,7 @@
 # Check for the existence of a venv directory and activate the Python virtual environment
 # If a venv or .venv directory is found, activate the corresponding virtual environment
 _check_venv() {
+    _deactivate_venv
     # Check for the existence of a venv or .venv directory
     if [ -d "venv" ]; then
         # Activate the venv virtual environment
@@ -11,12 +12,11 @@ _check_venv() {
         source .venv/bin/activate
         echo "Activated Python virtual environment: .venv"
     fi
-
-    current_dir="$PWD"
 }
 
 _deactivate_venv() {
-    if [ -n "$VIRTUAL_ENV" ]; then
+    local current_dir="${1-${PWD}}"
+    if [[ -v VIRTUAL_ENV ]] && [[ "${current_dir}" != "$(dirname ${VIRTUAL_ENV})"* ]]; then
         deactivate
         echo "Deactivated Python virtual environment: $current_dir"
     fi
@@ -24,29 +24,6 @@ _deactivate_venv() {
 # Change directory and automatically check and activate the Python virtual environment
 # This function will call the check_venv() function after changing the directory
 cd() {
-    # Init
-    # Get the target directory path
-    if [ "$#" -eq 0 ]; then
-        # No arguments, go to the user's home directory
-        target_dir="$HOME"
-    else
-        target_dir=$(realpath "$1" 2>/dev/null)
-        if [ "$?" -ne 0 ]; then
-            # Target directory does not exist
-            echo "cd: no such file or directory: $1"
-            return 1
-        fi
-    fi
-    # echo "target_dir: $target_dir"
-
-    if [ -z "$current_dir" ]; then
-        current_dir="$PWD"
-    fi
-
-    if [ "$target_dir" != "$current_dir" ]; then
-        _deactivate_venv
-    fi
-
     # Call the native cd command to change the directory
     builtin cd "$@"
 
